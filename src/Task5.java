2ab7c8d384edd0736ca684c2a6e5d9260f417807
asana <asanali1903@gmail.com>
2026-03-26 17:10:26 +0500

    task5

--- src/Task5.java
+++ src/Task5.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fib(n));
+    }
+
+    public static int fib(int n) {
+        if (n <= 1) return n;
+        return fib(n - 1) + fib(n - 2);
+    }
 }
