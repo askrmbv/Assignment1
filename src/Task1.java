69943915319e98d2b31147981419e23b996e1103
asana <asanali1903@gmail.com>
2026-03-26 17:04:32 +0500

    task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        printDigits(n);
+    }
+
+    public static void printDigits(int n) {
+        if (n == 0) return;
+
+        printDigits(n / 10);
+        System.out.println(n % 10);
+    }
 }
