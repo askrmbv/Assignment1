717bce716c60f3197588f0700a64aa7a4e6fc4c0
asana <asanali1903@gmail.com>
2026-03-26 17:08:07 +0500

    task2

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = sum(sc, n);
+        System.out.println((double) sum / n);
+    }
+
+    public static int sum(Scanner sc, int n) {
+        if (n == 0) return 0;
+        return sc.nextInt() + sum(sc, n - 1);
+    }
 }
