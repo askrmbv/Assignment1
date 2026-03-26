22275a60b2afd910ee311a6bb7afd65fa188aea3
asana <asanali1903@gmail.com>
2026-03-26 17:11:48 +0500

    task8

--- src/Task8.java
+++ src/Task8.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(check(s, 0) ? "Yes" : "No");
+    }
+
+    public static boolean check(String s, int i) {
+        if (i == s.length()) return true;
+        if (!Character.isDigit(s.charAt(i))) return false;
+
+        return check(s, i + 1);
+    }
 }
